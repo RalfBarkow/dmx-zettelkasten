4eba29b2f87d1c01e3311aa8447a2e65b3e84d73
Ralf Barkow <>
2020-11-26 01:33:21 +0100

    Overwrite with dmx-plug-template-master but keep migrations for Zettel

--- src/main/js/plugin.js
+++ src/main/js/plugin.js
@@ -1,26 +1,29 @@
-// Every DMX plugin which extends the DMX webclient has a `src/main/js/plugin.js` file.
+// Every DMX plugin which extends the DMX Webclient has a `src/main/js/plugin.js` file.
 // This file exports a DMX plugin configuration object or a function which returns such an object.
 // The function receives a "dependencies" object with 4 properties: 'store', 'dm5', 'axios', and 'Vue'.
-// 'store' is the DMX webclient's Vuex store object.
-export default ({ store, dm5, axios, Vue }) => ({
+// 'store' is the DMX Webclient's Vuex store object.
+export default ({store, dm5, axios, Vue}) => ({
 
   // A DMX plugin can provide its own Vuex store module.
-  // This plugin's state is accessible as 'store.state.zettel' (according to the 'name' property).
+  // This plugin's state is accessible as 'store.state.greeting' (according to the 'name' property).
   storeModule: {
-    name: 'zettel',
-    module: require('./zettel').default
+    name: 'greeting',
+    module: require('./greeting').default
   },
 
-  // A DMX plugin can statically add Vue components to the DMX webclient.
+  // A DMX plugin can statically add Vue components to the DMX Webclient.
   // 3 mount points are supported: 'webclient', 'toolbar-left', and 'toolbar-right'
   components: [{
-    comp: require('./components/Zettel').default,
-    mount: 'webclient'
+    comp: require('./components/Greeting').default,
+    mount: 'toolbar-left'
   }]
 
   // A DMX plugin can do more things at the client-side (not demonstrated here):
   // - declare Vuex store watchers
   // - provide type-specific detail renderers
   // - provide additional topicmap types and renderers
-  // - add special items to the webclient's Create menu
+  // - add special items to the Webclient's Create menu
+
+  // Learn more about DMX plugin development with the DMX Developer Guide:
+  // https://dmx.readthedocs.io/en/latest/devel.html
 })
