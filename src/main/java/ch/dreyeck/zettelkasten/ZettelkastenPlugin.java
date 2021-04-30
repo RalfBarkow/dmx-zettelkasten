a404e9a2ebe2dca32a3da0f5bc6a2d32c65dfc10
Ralf Barkow <>
2021-04-30 15:14:58 +0200

    npm i @croquet/croquet

--- src/main/java/ch/dreyeck/zettelkasten/ZettelkastenPlugin.java
+++ src/main/java/ch/dreyeck/zettelkasten/ZettelkastenPlugin.java
@@ -2,20 +2,10 @@ package ch.dreyeck.zettelkasten;
 
 import systems.dmx.core.osgi.PluginActivator;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
-
-import java.util.Iterator;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
